7abbd12123bde0f7b3be02507875e84a7d68b0fa
vizkey <micmin0101@gmail.com>
2026-03-18 15:14:06 +0700

    redeploy

--- api/ai.py
+++ api/ai.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-# Add the ai_service directory to sys.path
+# Adding the ai_service directory to sys.path
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "ai_service"))
 
 from main import app
